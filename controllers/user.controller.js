5d044009dfc8d8935e2c9425d8b83cb009c686a3
rasoolabdi <M0rd00r@yahoo.com>
2025-05-12 09:38:12 -0700

    added html and layouts pages login,register,recovery

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,6 +6,15 @@ class UserController extends BaseController {
         super();
     };
 
+    async home(req , res , next) {
+        try {
+            return res.render("pages/home.html")
+        }
+        catch(error) {
+            next(error);
+        }
+    }
+
     async index(req , res , next) {
         try {
             return res.render("layout/index.html");
@@ -14,5 +23,42 @@ class UserController extends BaseController {
             next(error)
         }
     }
+
+    async login(req , res , next) {
+        try {
+            const data = {
+                title: "ورود"
+            }
+            return res.render("pages/login.html" , data)
+        }
+        catch(error) {
+            next(error)
+        }
+    }
+
+    async register(req , res , next) {
+        try {
+            const data = {
+                title: "ثبت نام"
+            }
+            return res.render("pages/register.html" , data);
+        }
+        catch(error) {
+            next(error);
+        }
+    };
+
+    async recovery(req , res , next) {
+        try {
+            const data = {
+                title: "بازیابی کلمه عبور"
+            }
+            return res.render("pages/recovery.html" , data)
+        }
+        catch(error) {
+            next(error);
+        }
+    }
+
 };
 export default new UserController();
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,11 @@ import userController from "../controllers/user.controller.js";
 
 const userRouters = Router();
 
+userRouters.get("/" , userController.home);
 userRouters.get("/profile" , userController.index);
+userRouters.get("/login" , userController.login);
+userRouters.get("/register" , userController.register);
+userRouters.get("/recovery" , userController.recovery);
+
 
 export default userRouters;
